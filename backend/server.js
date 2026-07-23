f000ccccd7c220c7850552653bf2ab91163cc498
shafiya <khanshafiya1219@gmail.com>
2026-07-23 15:30:48 +0530

    setup mongoDB atlas connection

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
-const app = require('./src/app');
 require('dotenv').config();
+const app = require('./src/app');
+const connectDB = require('./src/config/db');
+
+connectDB();
+
 
 const PORT = process.env.PORT || 5000;
 
