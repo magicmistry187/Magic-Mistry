b61dc8fb1ac563195afbd220456383cb0bdc71bd
shafiya <khanshafiya1219@gmail.com>
2026-08-11 18:09:31 +0530

    Update imagekit config

--- backend/src/config/imagekit.js
+++ backend/src/config/imagekit.js
@@ -6,11 +6,11 @@ const imagekit = new ImageKit({
   privateKey: process.env.ImageKit_Pirvate_Key,
 });
 
-async function uploadImageToImageKit(buffer) {
+async function uploadImageToImageKit(buffer,fileName) {
   // console.log("in the imagekit")
   const result = await imagekit.files.upload({
     file: buffer.toString('base64'),
-    fileName: 'image.jpg',
+    fileName: fileName, 
   });
 
   // console.log("image result is", result)
